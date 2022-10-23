b4b9397974307cd9c0f1bcbdb2a72b1197496248
Miles Straessle <miles.straessle@protonmail.com>
2022-10-23 23:47:12 +0200

    WiP

--- init.el
+++ init.el
@@ -37,7 +37,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(ace-window switch-window doom-modeline doom-themes use-package-ensure-system-package delight)))
+   '(ace-window switch-window doom-modeline doom-themes use-package-ensure-system-package delight))
+ '(paradox-github-token t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
---
+++ org-clock-save.el
@@ -0,0 +1,1 @@
+;; org-clock-save.el - raute at <2022-10-23 Sun 23:45>
