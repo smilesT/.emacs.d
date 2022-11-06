5dfa73e2adeb09a71849002f6d9d55f9b496c56d
Miles Straessle <miles.straessle@protonmail.com>
2022-11-06 03:28:01 +0100

    Performance GC

--- init.el
+++ init.el
@@ -1,15 +1,49 @@
+;;;;; Startup optimizations
 
-;; Avoid garbage collection at statup
-(setq gc-cons-threshold most-positive-fixnum ; 2^61 bytes
-      gc-cons-percentage 0.6)
+;;;;;; Set garbage collection threshold
+
+;; From https://www.reddit.com/r/emacs/comments/3kqt6e/2_easy_little_known_steps_to_speed_up_emacs_start/
+
+(setq gc-cons-threshold-original gc-cons-threshold)
+(setq gc-cons-threshold (* 1024 1024 100))
+
+;;;;;; Set file-name-handler-alist
+
+;; Also from https://www.reddit.com/r/emacs/comments/3kqt6e/2_easy_little_known_steps_to_speed_up_emacs_start/
+
+(setq file-name-handler-alist-original file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
+;;;;;; Set deferred timer to reset them
+
+(run-with-idle-timer
+ 5 nil
+ (lambda ()
+   (setq gc-cons-threshold gc-cons-threshold-original)
+   (setq file-name-handler-alist file-name-handler-alist-original)
+   (makunbound 'gc-cons-threshold-original)
+   (makunbound 'file-name-handler-alist-original)
+   (message "gc-cons-threshold and file-name-handler-alist restored")))
+
+
+
+
+
+
+
+
+
+;; ;; Avoid garbage collection at statup
+;; (setq gc-cons-threshold most-positive-fixnum ; 2^61 bytes
+;;       gc-cons-percentage 0.6)
 
 ;; All the settings and package installation is set in configuration.org
 (org-babel-load-file "~/.emacs.d/config.org")
 
-(add-hook 'emacs-startup-hook
-  (lambda ()
-    (setq gc-cons-threshold 300000000 ; 300mb
-          gc-cons-percentage 0.1)))
+;; (add-hook 'emacs-startup-hook
+;;   (lambda ()
+;;     (setq gc-cons-threshold 300000000 ; 300mb
+;;           gc-cons-percentage 0.1)))
 
 ;; (custom-set-variables
 ;;  ;; custom-set-variables was added by Custom.
@@ -31,17 +65,18 @@
 ;;  ;; Your init file should contain only one such instance.
 ;;  ;; If there is more than one, they won't work right.
 ;;  )
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(ace-window switch-window doom-modeline doom-themes use-package-ensure-system-package delight))
- '(paradox-github-token t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; (custom-set-variables
+;;  ;; custom-set-variables was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(package-selected-packages
+;;    '(ace-window switch-window doom-modeline doom-themes use-package-ensure-system-package delight))
+;;  '(paradox-github-token t)
+;;  '(warning-suppress-types '((comp))))
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  )
